68b12bce4c9f35e731af3b32c89f2e9fb78af778
Jhampier Tucno <yantucnoperez@gmail.com>
2024-12-13 14:28:51 -0500

    Add metadata to DashboardLayout and persist sidebar state in cookies; enhance page content with additional text

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,22 +2,31 @@ import { Header } from "@/components/layout/navbar/header";
 import { AppSidebar } from "@/components/layout/sidenav/app-sidebar";
 
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Metadata } from "next";
+import { cookies } from "next/headers";
 
-export default function DashboardLayout({
+export const metadata: Metadata = {
+  title: "Next Shadcn Dashboard Starter",
+  description: "Basic dashboard with Next.js and Shadcn",
+};
+
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Persisting the sidebar state in the cookie.
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
+
   return (
-    <SidebarProvider className="font-[family-name:var(--font-inter)]">
+    <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar />
 
       <SidebarInset>
         <Header />
 
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          {children}
-        </div>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,49 +1,46 @@
-"use client";
-
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export default function Page() {
-  const { setTheme } = useTheme();
-
   return (
     <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
       </div>
       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+
+      <div className="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui incidunt reprehenderit non odio autem delectus, illo accusamus sunt minima suscipit illum dolore earum, vel omnis in, rem soluta eveniet.</p>
+      </div>
     </>
   );
 }
