bbf454569721ff860fc6d8463f6671139f098613
Jhampier Tucno <yantucnoperez@gmail.com>
2024-12-27 19:33:45 -0500

    Add new UI components including Calendar, Checkbox, Switch, RadioGroup, and Badge; implement mail and task data management

--- src/app/dashboard/examples/ui/page.tsx
+++ src/app/dashboard/examples/ui/page.tsx
@@ -1,8 +1,34 @@
+import { CardsDemo } from "@/components/cards-ui";
+import { Metadata } from 'next';
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header";
+
+export const metadata: Metadata = {
+  title: "Card UI components",
+  description: "Beautifully designed components that you can copy and paste into your apps. Made with Tailwind CSS. Open source.",
+};
 
 export default function Page() {
   return (
-    <div>
-      <h1>UI Page</h1>
-    </div>
+    <>
+      <PageHeader>
+        <PageHeaderHeading>Card UI components</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your
+          apps. Made with Tailwind CSS. Open source.
+        </PageHeaderDescription>
+      </PageHeader>
+
+      <div className="container-wrapper">
+        <div className="container py-6">
+          <section className="[&>div]:p-0">
+            <CardsDemo />
+          </section>
+        </div>
+      </div>
+    </>
   );
 }
